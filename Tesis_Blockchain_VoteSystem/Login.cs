8356a98f540891a25121ec8f6e15dacf2a8eb0a1
Daniel Antonio Gallardo Muñoz <daniel.gallardo.munoz@gmail.com>
2021-04-17 11:49:39 -0400

    Agregar archivos de proyecto.

---
+++ Tesis_Blockchain_VoteSystem/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tesis_Blockchain_VoteSystem
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
